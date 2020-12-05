c1a0d5bf6ac1f3f5ccecbaf31d9179efdbf29602
Arunas Dzedulionis <36657198+adzedas63@users.noreply.github.com>
2020-12-05 23:25:49 +0200

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -29,6 +29,31 @@ $(function () {
 
 
 
+/* Modalinis langas */
+    let btns = document.querySelectorAll("*[data-modal-btn]");
+
+    for (let i = 0; i < btns.length; i++) {
+        btns[i].addEventListener('click', function () {
+            let name = btns[i].getAttribute('data-modal-btn');
+            let modal = document.querySelector("[data-modal-window='" + name + "']");
+            modal.style.display = "block";
+            let close = modal.querySelector(".close__modal__window");
+            close.addEventListener('click', function () {
+                modal.style.display = "none";
+            })
+        })
+    }
+    
+    window.onclick = function (e) {
+        if (e.target.hasAttribute('data-modal-window')) {
+            let modals = document.querySelectorAll("*[data-modal-window]");
+            for (let i = 0; i < modals.length; i++) {
+                modals[i].style.display = "none";
+            }
+        }
+    }
+    
+
     /* Modal
     =====================*/
 
@@ -134,3 +159,17 @@ $(function () {
 
 
 });
+
+
+// Create a JavaScript Date object for the current date and time, and set the desired options.
+var date = new Date(),
+options = { weekday: "long", month: "long", day: "numeric", year: "numeric" };
+
+// Convert to locale string and add a locale and the options
+date = date.toLocaleString( "en-US", options );
+
+// Output the date to the above HTML element
+document.getElementById("msg").innerHTML = date;
+
+
+
